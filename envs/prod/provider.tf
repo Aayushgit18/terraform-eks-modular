c6e9212f1baede8e74c324e98be88023ea7b9504
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-04 16:34:42 +0530

    Update provider.tf

--- envs/prod/provider.tf
+++ envs/prod/provider.tf
@@ -1,4 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "ayush-terraform-state"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = var.region
 }
-
