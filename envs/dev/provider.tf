bb008b8b6697df4b44f382ae835591b637613233
Aayushgit18 <aayushchoundiye@gmail.com>
2025-12-04 16:35:06 +0530

    Modify Terraform backend configuration for dev
    Updated S3 backend configuration for Terraform.

--- envs/dev/provider.tf
+++ envs/dev/provider.tf
@@ -1,10 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "ayush-terraform-state"
-    key            = "eks/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket = "ayush-terraform-state"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
